cfdb516133cc8a7bd40aab7c253ed0f36c2547c5
Mukesh Suthar <mukeshsuthar336@gmail.com>
2019-06-02 19:57:25 +0530

    bootstrap bug fixed
    panels was absoluted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BlogViewComponent,
-    BlogCreateComponent
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from "@angular/router";
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
   styleUrls: ['./blog-view.component.css']
 })
 export class BlogViewComponent implements OnInit {
+  public currentBlog;
 
-  constructor() { }
+  public allBlogs = [{
+    Id: 1,
+    Name: "Blog1",
+    Description: "Desc1",
+    Author: "xyz"
+  },
+  {
+    Id: 2,
+    Name: "Blog2",
+    Description: "Desc2",
+    Author: "abc"
+  },
+  {
+    Id: 3,
+    Name: "Blog 3",
+    Description: "Desc 3",
+    Author: "xyz"
+  }]
 
-  ngOnInit() {
+  constructor(private _route: ActivatedRoute, private router: Router) {
+
   }
 
+  ngOnInit() {
+    let blogId = this._route.snapshot.paramMap.get("blogId");
+    this.currentBlog = this.getCurrentBlog(blogId);
+    console.log(this.currentBlog);
+  }
+
+  public getCurrentBlog = function (blogId) {
+    for (let i in this.allBlogs) {
+      if (this.allBlogs[i].Id == Number(blogId)) {
+        return this.allBlogs[i];
+      }
+    }
+  }
 }
